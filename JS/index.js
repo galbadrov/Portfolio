39b55b9ebea1760785914a25d4274e9afa3f1066
GalBadrov <gal.badrov@student.um.si>
2025-05-05 21:18:53 +0200

    animacija, about me

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 140) {
+        if ($(window).scrollTop() > 300) {
             $('#ime').addClass('scrolled');
             $('.fade').addClass('fade-out');
         } else {
